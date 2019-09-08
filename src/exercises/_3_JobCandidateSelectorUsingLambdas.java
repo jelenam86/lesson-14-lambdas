e1eb518d7ee8d76dfdf7b254ca5c752a77f9b776
jelenam86 <jelena.mat86@gmail.com>
2019-09-08 21:02:15 +0200

    Sorted candidates

--- src/exercises/_3_JobCandidateSelectorUsingLambdas.java
+++ src/exercises/_3_JobCandidateSelectorUsingLambdas.java
@@ -1,14 +1,18 @@
 package exercises;
 
+import java.util.Collections;
+
 public class _3_JobCandidateSelectorUsingLambdas {
 	public static void main(String[] args) {
 
 		// 1. Sort the candidates by salary requirements, low to high.
-		
+		Collections.sort(JobCandidate.jobCandidates, (c1, c2) -> c1.getSalaryRequired() - c2.getSalaryRequired());
+		System.out.println(JobCandidate.jobCandidates);
 
 		// 2. Filter out any candidates that have not been taught by me (June)
 		// Hint: see the removeIf() method in the Collections class
-		
-		
+		JobCandidate.jobCandidates.removeIf(c -> !c.getTeacherName().contains("June"));
+		System.out.println(JobCandidate.jobCandidates);
+
 	}
 }
